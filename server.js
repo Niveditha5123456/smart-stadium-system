c08c754854b12b8dbe607e9cbe6d229909fc25fa
Niveditha5123456 <166196174+Niveditha5123456@users.noreply.github.com>
2026-04-20 07:49:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
